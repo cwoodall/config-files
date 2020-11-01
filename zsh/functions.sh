655c522d897e99fee36a7b4d6df9e53d119fb521
Chris Woodall <chris.j.woodall@gmail.com>
2020-11-01 18:08:06 -0500

    Change workspace and switch workspace

---
+++ zsh/functions.sh
@@ -0,0 +1,38 @@
+sws() {
+    export workspace_path=$(pwd)
+    echo $workspace_path
+
+    export workspace_name=""
+    while [[ $(pwd) != $HOME ]]; do
+        if [[ -d ./.venv ]] then
+            . ./.venv/bin/activate
+            echo "Found .venv in $(pwd)"
+            break
+        fi
+        cd ..
+    done
+    
+    cd $workspace_path
+
+    while [[ $(pwd) != $HOME ]]; do
+        if [[ -f .env ]] then
+            . ./.env
+            echo "Found .env in $(pwd)"
+            break
+        fi
+        cd ..
+    done
+
+    cd $workspace_path
+
+    if [[ $workspace_name ]] then
+        echo "Loading $workspace_name"
+        export PROMPT="($workspace_name) $PROMPT"
+    fi
+}
+
+chws() {
+    cd ${WORKSPACE_DIR}/${1:-}
+    sws
+}
+
